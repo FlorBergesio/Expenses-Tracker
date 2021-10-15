8e230fd0ba47fe7a51f5ad8bcb7c9363e5364ada
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-15 13:09:23 -0300

    Started working on the filter component

--- src/components/Expenses/ExpensesContainer.js
+++ src/components/Expenses/ExpensesContainer.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
 import './ExpensesContainer.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 const ExpensesContainer = ( { expenses } ) => {
+  const [ filteredYear, setFilteredYear ] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear( selectedYear );
+    console.log( selectedYear );
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter 
+        onChangeFilter={ filterChangeHandler } 
+        selected={ filteredYear }
+      />
+
       {
         expenses.map( item => (
           <ExpenseItem
