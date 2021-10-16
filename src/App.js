ad89bce732800e088c532c6c0e84b0315d59c717
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-16 11:42:41 -0300

    Expenses list is now a state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
+import { useState } from "react";
+
 import ExpensesContainer from "./components/Expenses/ExpensesContainer";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initialExpenses = [
+  {
+    id: "e1",
+    title: "PC mouse",
+    amount: 979.0,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Groceries",
+    amount: 1842.7,
+    date: new Date(2021, 5, 4),
+  },
+  {
+    id: "e3",
+    title: "Doctor's appointment",
+    amount: 1500.0,
+    date: new Date(2021, 10, 4),
+  },
+  {
+    id: "e4",
+    title: "Presents",
+    amount: 2451.99,
+    date: new Date(2021, 12, 1),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "PC mouse",
-      amount: 979.0,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Groceries",
-      amount: 1842.7,
-      date: new Date(2021, 5, 4),
-    },
-    {
-      id: "e3",
-      title: "Doctor's appointment",
-      amount: 1500.0,
-      date: new Date(2021, 10, 4),
-    },
-    {
-      id: "e4",
-      title: "Presents",
-      amount: 2451.99,
-      date: new Date(2021, 12, 1),
-    },
-  ];
+  const [ expenses, setExpenses ] = useState( initialExpenses );
 
   const addExpenseHandler = expense => {
-    console.log("in app.js", expense)
+    setExpenses( prevState => ( [ expense, ...prevState ] ) )
   };
 
   return (
