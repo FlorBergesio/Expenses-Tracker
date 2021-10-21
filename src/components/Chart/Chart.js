fc7ef207fa9eb96d7fa9473338277be582d01d56
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-21 11:42:03 -0300

    Added components to manage and display total expenses amounts by month

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,24 @@
+import './Chart.css';
+import ChartBar from "./ChartBar";
+
+const Chart = ( { dataPoints } ) => {
+  const dataPointValues = dataPoints.map( dataPoint => dataPoint.value);
+  const totalMax = Math.max( ...dataPointValues );
+
+  return (
+    <div className="chart">
+      {
+        dataPoints.map( dataPoint => (
+          <ChartBar 
+            key={ dataPoint.label }
+            value={ dataPoint.value }
+            maxValue={ totalMax }
+            label={ dataPoint.label }
+          />
+        ) )
+      }
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/Expenses/ExpensesContainer.js
+++ src/components/Expenses/ExpensesContainer.js
@@ -4,6 +4,7 @@ import './ExpensesContainer.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const ExpensesContainer = ( { expenses } ) => {
   const [ filteredYear, setFilteredYear ] = useState('2021');
@@ -23,7 +24,7 @@ const ExpensesContainer = ( { expenses } ) => {
         onChangeFilter={ filterChangeHandler } 
         selected={ filteredYear }
       />
-
+      <ExpensesChart expenses={ filteredExpenses } />
       <ExpensesList expenses={ filteredExpenses } />
     </Card>
   );
